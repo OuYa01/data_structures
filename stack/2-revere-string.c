af6c1341cd263f55b4bcdf8e7b06e738e5a5cfd1
ouya01 <ouya01@gmail.com>
2024-11-02 12:38:53 +0100

    i add comments to my code to be more understod

--- stack/2-revere-string.c
+++ stack/2-revere-string.c
@@ -3,18 +3,35 @@
 
 int top = -1;
 
+/**
+ * push - to push element to the top of stack
+ * @s: string that i will push element in it (my stack)
+ * @x: the element that will be pushed to my stack
+ *
+ * Return: void
+ */
 void push(char* s, char x)
 {
     top = top + 1;
     s[top] = x;
 }
 
+/**
+ * pop - pop the top element in stack
+ * @s: stack
+ *
+ * Return: the top element that will be poped
+ */
 char pop(char* s)
 {
     return (s[top--]);
 }
 
-
+/**
+ * reverse - reverse string using stack
+ * @s: the string to be resversed
+ * @size: size of string
+ */
 void reverse(char* s, int size)
 {
     char stack[size];
@@ -30,6 +47,12 @@ void reverse(char* s, int size)
         s[i] = pop(stack);
     }
 }
+
+/**
+ * main - The entry point
+ *
+ * Return: 0 if succ\
+ */
 int main(void)
 {
     char s[] = "hello";
@@ -39,5 +62,5 @@ int main(void)
     reverse(s, size);
     printf("the string after revers it: %s\n", s);
 
+    return (0);
 }
-
